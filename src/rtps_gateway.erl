49c1f7d7db627c005dda5dc87a1a38f3b4a617f7
Luca Succi <luca.succi@stritzinger.com>
2025-04-04 10:26:13 +0200

    Send all sendable fragments to speed up comunications

--- src/rtps_gateway.erl
+++ src/rtps_gateway.erl
@@ -8,7 +8,8 @@
 -behaviour(gen_server).
 -export([init/1, handle_cast/2, handle_call/3]).
 
--include("rtps_structure.hrl").
+-include_lib("kernel/include/logger.hrl").
+-include("../include/rtps_structure.hrl").
 
 -record(state, {participant = #participant{}, socket}).
 
@@ -36,9 +37,9 @@ handle_call(_, _, State) ->
 handle_cast({send, {Datagram, Dst}}, #state{socket = S} = State) ->
     case gen_udp:send(S, Dst, Datagram) of
         ok -> ok;
-        {error, Reason} -> 
-                        io:format("[GATEWAY]: failed sending to: ~p\n",[Dst]),
-                        io:format("[GATEWAY]: reason of failure: ~p\n",[Reason])
+        {error, Reason} ->
+            ?LOG_ERROR("[GATEWAY]: failed sending to: ~p\n",[Dst]),
+            ?LOG_ERROR("[GATEWAY]: reason of failure: ~p\n",[Reason])
     end,
     {noreply, State};
 handle_cast(_, State) ->
